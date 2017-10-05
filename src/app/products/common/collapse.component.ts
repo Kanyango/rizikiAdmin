9bc7619755b26106679263417f21fad83cc231f7
Kanyango <kariukikanyango@gmail.com>
2017-10-05 21:12:30 +0300

    Update collapse.component.ts

--- src/app/products/common/collapse.component.ts
+++ src/app/products/common/collapse.component.ts
@@ -17,7 +17,7 @@ export class CollapseComponent implements OnInit{
   public isCollapsed = false;
   private headers = new Headers({'Content-Type': 'application/json'});
   //private productsUrl = 'http://127.0.0.1:8300/upload';
-  private URL = ' https://rizikisever.herokuapp.com/upload';
+  private URL = ' https://rizikisever.herokuapp.com/upload/';
   constructor(public http: Http, private el: ElementRef){}
 
 
@@ -38,7 +38,7 @@ export class CollapseComponent implements OnInit{
                 formData.append('photo', inputEl.files.item(i));
             }
             this.http
-                .post(this.URL, formData).map((res:any) => res).subscribe(
+                .put(this.URL, + id , formData).map((res:any) => res).subscribe(
                     (success) => {
                      alert(success._body);
                   },
