54feba0e7c6f4d3dcb12ea208a91e2bf3aaeecde
Kanyango <kariukikanyango@gmail.com>
2017-10-06 12:04:01 +0300

    Update collapse.component.ts

--- src/app/products/common/collapse.component.ts
+++ src/app/products/common/collapse.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ElementRef} from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise'
-//import { FileUploader } from 'ng2-file-upload';
+import { FileUploader } from 'ng2-file-upload';
 
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
@@ -17,36 +17,16 @@ export class CollapseComponent implements OnInit{
   public isCollapsed = false;
   private headers = new Headers({'Content-Type': 'application/json'});
   //private productsUrl = 'http://127.0.0.1:8300/upload';
-  private URL = ' https://rizikisever.herokuapp.com/upload/';
+  //private URL = ' https://rizikisever.herokuapp.com/upload/';
+  public uploader:FileUploader = new FileUploader(
+      {url:'https://rizikisever.herokuapp.com/upload/id', itemAlias: 'photo' , prodId: prodId});
+  
   constructor(public http: Http, private el: ElementRef){}
 
 
   ngOnInit()
   {
     console.log(this.one);
-  }
-
-  upload(id: any)
-  {
-
-    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#photo');
-        console.log("iam+ "+inputEl);
-        let fileCount: number = inputEl.files.length;
-        let formData = new FormData();
-        if (fileCount > 0) { // a file was selected
-            for (let i = 0; i < fileCount; i++) {
-                formData.append('photo', inputEl.files.item(i));
-            }
-            console.log(formData);
-            this.http
-                .put(this.URL, + id , formData).map((res:any) => res).subscribe(
-                    (success) => {
-                     alert(success._body);
-                  },
-                    (error) => alert(error)
-                );
-
-        }
   }
 
 
