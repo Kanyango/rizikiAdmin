0355a0256560199e5e8d857424a47c3862e32685
Kanyango <kariukikanyango@gmail.com>
2017-10-07 14:20:15 +0300

    Update collapse.component.ts

--- src/app/products/common/collapse.component.ts
+++ src/app/products/common/collapse.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, Input, EventEmitter} from '@angular/core';
 import { Headers, Http } from '@angular/http';
-import {UPLOAD_DIRECTIVES} from 'ng2-file-uploader/ng2-file-uploader';
+import { UploadOutput, UploadInput, UploadFile, humanizeBytes, UploaderOptions } from 'ngx-uploader';
 import 'rxjs/add/operator/toPromise'
-
-
 
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
@@ -17,7 +15,18 @@ import "rxjs/add/operator/map";
 export class CollapseComponent{
 
   @Input() one;
+  options: UploaderOptions;
+  formData: FormData;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  humanizeBytes: Function;
+  dragOver: boolean;
 
+  constructor() {
+    this.files = []; // local uploading files array
+    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
+    this.humanizeBytes = humanizeBytes;
+  }
   
   public isCollapsed = false;
   private headers = new Headers({'Content-Type': 'application/json'});
@@ -25,18 +34,44 @@ export class CollapseComponent{
   
   
 
-  handleUpload(data, id, prodName): void {
-    options: Object = {
-        url: `${this.uploadUrl}${id}`,
-        method: 'PUT',
-        data: {'prodName': prodName}
-      };
-    if (data && data.response) {
-      data = JSON.parse(data.response);
-      this.uploadFile = data;
+  onUploadOutput(output: UploadOutput): void {
+    if (output.type === 'allAddedToQueue') { // when all files added in queue
+      // uncomment this if you want to auto upload files when added
+      // const event: UploadInput = {
+      //   type: 'uploadAll',
+      //   url: '/upload',
+      //   method: 'POST',
+      //   data: { foo: 'bar' }
+      // };
+      // this.uploadInput.emit(event);
+    } else if (output.type === 'addedToQueue'  && typeof output.file !== 'undefined') { // add file to array when added
+      this.files.push(output.file);
+    } else if (output.type === 'uploading' && typeof output.file !== 'undefined') {
+      // update current data in files array for uploading file
+      const index = this.files.findIndex(file => typeof output.file !== 'undefined' && file.id === output.file.id);
+      this.files[index] = output.file;
+    } else if (output.type === 'removed') {
+      // remove file from array when removed
+      this.files = this.files.filter((file: UploadFile) => file !== output.file);
+    } else if (output.type === 'dragOver') {
+      this.dragOver = true;
+    } else if (output.type === 'dragOut') {
+      this.dragOver = false;
+    } else if (output.type === 'drop') {
+      this.dragOver = false;
     }
   }
-  
+
+  startUpload(): void {
+    const event: UploadInput = {
+      type: 'uploadAll',
+      url: 'http://ngx-uploader.com/upload',
+      method: 'POST',
+      data: { foo: 'bar' }
+    };
+
+    this.uploadInput.emit(event);
+  }
   
   
 }
