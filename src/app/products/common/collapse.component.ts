01318ccd7f6db4addd64595ece946c1d47cd8c16
Kanyango <kariukikanyango@gmail.com>
2017-10-07 14:01:48 +0300

    Update collapse.component.ts

--- src/app/products/common/collapse.component.ts
+++ src/app/products/common/collapse.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input, EventEmitter} from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise'
-//import { FileUploader } from 'ng2-file-upload';
-import { UploadOutput, UploadInput, UploadFile, humanizeBytes, UploaderOptions } from 'ngx-uploader';
+
 
 
 import "rxjs/add/operator/do";
@@ -16,12 +15,7 @@ import "rxjs/add/operator/map";
 export class CollapseComponent{
 
   @Input() one;
-  options: UploaderOptions;
-  formData: FormData;
-  files: UploadFile[];
-  uploadInput: EventEmitter<UploadInput>;
-  humanizeBytes: Function;
-  dragOver: boolean;
+  
   
   constructor() {
     this.files = []; // local uploading files array
@@ -33,48 +27,4 @@ export class CollapseComponent{
   private headers = new Headers({'Content-Type': 'application/json'});
   private uploadUrl = 'https://rizikisever.herokuapp.com/upload/';
   
-   onUploadOutput(output: UploadOutput, id, prodId): void {
-     console.log(output.file);
-    if (output.type === 'allAddedToQueue') { // when all files added in queue
-       //uncomment this if you want to auto upload files when added
-      //const URL = `${this.uploadUrl}${id}`;
-      //const event: UploadInput = {
-      //type: 'uploadAll',
-      //url: URL,
-      //method: 'PUT',
-      //data: { 'prodName' : prodId, }
-    //};
-    //this.uploadInput.emit(event);
-      //this.uploadInput.emit(event);
-    } else if (output.type === 'addedToQueue'  && typeof output.file !== 'undefined') { // add file to array when added
-      this.files.push(output.file);
-    } else if (output.type === 'uploading' && typeof output.file !== 'undefined') {
-      // update current data in files array for uploading file
-      const index = this.files.findIndex(file => typeof output.file !== 'undefined' && file.id === output.file.id);
-      this.files[index] = output.file;
-    } else if (output.type === 'removed') {
-      // remove file from array when removed
-      this.files = this.files.filter((file: UploadFile) => file !== output.file);
-    } else if (output.type === 'dragOver') {
-      this.dragOver = true;
-    } else if (output.type === 'dragOut') {
-      this.dragOver = false;
-    } else if (output.type === 'drop') {
-      this.dragOver = false;
-    }
-  }
-  
-  startUpload(id, prodName): void {
-    //console.log(output);
-    const URL = `${this.uploadUrl}${id}`;
-    const event: UploadInput = {
-      type: 'uploadAll',
-      url: URL,
-      method: 'PUT',
-      data: { 'prodName' : prodName }
-    };
-    this.uploadInput.emit(event);
-  }
    
-
-}
