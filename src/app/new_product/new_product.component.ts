73dae9d96096137d447e5aa420025c07dd80b46a
Kanyango <kariukikanyango@gmail.com>
2017-10-08 20:48:39 +0300

    Update new_product.component.ts

--- src/app/new_product/new_product.component.ts
+++ src/app/new_product/new_product.component.ts
@@ -27,6 +27,7 @@ export class NewProductComponent implements OnInit {
 
             brand: ['', Validators.required],
             category: ['',Validators.required],
+            sku: ['',Validators.required],
             variations: this.fb.array([])
     })
   }
